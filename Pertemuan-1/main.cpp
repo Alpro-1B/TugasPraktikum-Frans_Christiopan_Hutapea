a75106b662de011401bedbecf4780b7c680862b3
Bjirka13 <147828133+Bjirka13@users.noreply.github.com>
2023-10-14 11:40:19 +0700

    Add files via upload

---
+++ Pertemuan-1/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"-------------------------------------------------------\n";
+    cout<<"Nama: Frans Christiopan Hutapea\n\n";
+    cout<<"NPM: 2310631170086\n\n";
+    cout<<"Kelas: B - Informatika\n\n";
+    cout<<"Cita-Cita: Menuju Tak Terbatas dan melampuinya\n\n";
+    cout<<"Motivasi Masuk Informatika: Background Sepupu \n";
+    cout<<"-------------------------------------------------------\n\n\n";
+
+    return 0;
+}
